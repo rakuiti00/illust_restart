5ed66f02aed2f42b6d7d5e9599c4f18a0b6934e4
rakuiti00 <rakuiti77@gmail.com>
2021-10-08 21:54:36 +0900

    投稿内容編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,20 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end  
+  end
+
 
 
   private
