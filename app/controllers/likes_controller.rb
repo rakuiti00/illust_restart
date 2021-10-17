299b5921268bf0efb1ea8839a3a2c3e7d2b51ae9
rakuiti00 <rakuiti77@gmail.com>
2021-10-17 17:53:20 +0900

    内部設計に基づく修正⑥

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,28 @@
 class LikesController < ApplicationController
 
   def create
+      
     @like = Like.new(
       user_id: current_user.id,
       post_id: params[:post_id]
     )
     @like.save
-    redirect_to(post_path(params[:post_id]))   
+
+    post = Post.find(params[:post_id])
+
+    # 遷移元によって遷移先を変えたい
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "posts" && path[:action] == "show"
+      redirect_to(post_path(params[:post_id]))
+    elsif path[:controller] == "posts" && path[:action] == "index"
+      redirect_to(root_path)
+    elsif path[:controller] == "users" && path[:action] == "show"
+      redirect_to(user_path(post.user_id))
+    elsif path[:controller] == "tranings" && path[:action] == "index"
+      redirect_to(tranings_path)
+    else
+      redirect_to(root_path)
+    end
   end
 
   def destroy
@@ -15,7 +31,20 @@ class LikesController < ApplicationController
       post_id: params[:post_id]
     )
     @like.destroy
-    redirect_to(post_path(params[:post_id]))   
+
+    # 遷移元によって遷移先を変えたい
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "posts" && path[:action] == "show"
+      redirect_to(post_path(params[:post_id]))
+    elsif path[:controller] == "posts" && path[:action] == "index"
+      redirect_to(root_path)
+    elsif path[:controller] == "users" && path[:action] == "show"
+      redirect_to(user_path(post.user_id))
+    elsif path[:controller] == "tranings" && path[:action] == "index"
+      redirect_to(tranings_path)
+    else
+      redirect_to(root_path)
+    end
   end
 
 end
