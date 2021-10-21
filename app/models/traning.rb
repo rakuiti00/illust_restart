76f578ce4c5667472aa3d56d599ee20ae6caf114
rakuiti00 <rakuiti77@gmail.com>
2021-10-21 16:40:15 +0900

    部分テンプレート化の実装

--- app/models/traning.rb
+++ app/models/traning.rb
@@ -2,8 +2,6 @@ class Traning < ApplicationRecord
 
   validates :theme_text, presence: true
 
-  # belongs_to  :user
-  # belongs_to  :post
-  has_one_attached :image
+  has_one_attached :image, dependent: :destroy
   has_many :posts
 end
