8ace364a0e89a13bf58fd08f36d346af90844c07
rakuiti00 <rakuiti77@gmail.com>
2021-10-21 19:42:29 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字と数字を混合してください" }
+
+  URL_REGEX = /https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+|\A\z/
+  with_options format: { with: URL_REGEX, message: '正しいURLを入力してください' } do
+    validates :twitter_url
+    validates :pixiv_url
+    validates :another_url
+  end
+
   
   has_many :posts, dependent: :destroy
   has_many :likes
