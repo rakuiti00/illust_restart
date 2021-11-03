b7eb830f1f4a1626f874a73821f9b518a005b83b
rakuiti00 <rakuiti77@gmail.com>
2021-11-03 16:31:39 +0900

    ビューの修正、不要な記述の削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,8 +9,6 @@ class LikesController < ApplicationController
 
     @post = Post.find(params[:post_id])
 
-    # return_select
-
   end
 
   def destroy
@@ -22,27 +20,6 @@ class LikesController < ApplicationController
 
     @post = Post.find(params[:post_id])
 
-    # return_select
   end
 
-  private
-
-  def return_select
-
-    path = Rails.application.routes.recognize_path(request.referer)
-    if path[:controller] == "posts" && path[:action] == "show"
-      redirect_to(post_path(params[:post_id]))
-    elsif path[:controller] == "posts" && path[:action] == "index"
-      # redirect_to(root_path)
-    elsif path[:controller] == "users" && path[:action] == "show"
-      redirect_to(user_path(@post.user_id))
-    elsif path[:controller] == "tranings" && path[:action] == "index"
-      redirect_to(tranings_path)
-    elsif path[:controller] == "posts" && path[:action] == "search"
-      redirect_to(search_posts_path)
-    else
-      # redirect_to(root_path)
-    end    
-  
-  end
 end
