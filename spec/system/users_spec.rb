896909f166c136f42e83628a9556e14f692d1e6d
rakuiti00 <rakuiti77@gmail.com>
2021-12-09 14:59:25 +0900

    結合テスト 新規登録できること

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,63 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "ユーザー新規登録", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do
+    it '正しい情報を入力すれば、ユーザの新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+
+      #トップページに新規登録へ遷移するリンク（文字）があることを確認する
+      expect(page).to have_content('新規登録')
+
+      #新規登録ページへ移動
+      visit new_user_registration_path
+
+      #ユーザ情報を入力
+      fill_in 'user_name', with: @user.nickname
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      fill_in 'user_password_confirmation', with: @user.password_confirmation
+
+      #登録ボタンを押下でユーザモデルのカウントが１上がることを確認(レコード)
+      expect{
+        find('input.sign_up').click
+      }.to change {User.count}.by(1)
+
+      #トップページへ遷移を確認
+      expect(current_path).to eq(root_path)
+
+      #ログアウトボタンが表示されていることを確認
+      expect(page).to have_content('ログアウト')
+
+      #サインアップ、ログインへ遷移するボタンが表示されていないことを確認
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ユーザー新規登録ができるとき' do
+    it '謝った情報では新規登録ができず、新規登録ページに戻る' do
+      #トップページに遷移
+      #visit root_path
+
+      #トップページに新規登録へ遷移するリンクがあることを確認する
+      #expect(page).to have_content('新規登録')
+
+      #新規登録ページへ移動
+      #visit new_user_registration_path
+
+      #ユーザ情報を入力
+
+
+      #登録ボタンを押下してもユーザモデルのカウントは上がらないことを確認
+
+      #新規登録ページへ戻されることを確認
+
+    end
+  end
+
 end
